3a0a87ac64f7ef657fc0c915fad968a4feb07792
漆谦 <i70000@dingtalk.com>
2019-12-31 23:44:44 +0800

    add README.md

--- CLionProjects/embedC/main.c
+++ CLionProjects/embedC/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+# pragma warning (disable:4819)
 
+//int main(int argc,char *argv[]) {
 int main() {
+//    if(argc < 2){
+        printf("请输入一个参数");
+//    }
     printf("Hello, World!\n");
     return 0;
 }

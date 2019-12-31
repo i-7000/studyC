bf4861d4be2afe7a98d428174a91aadc41506ee4
漆谦 <i70000@dingtalk.com>
2019-12-31 23:01:30 +0800

    inist project

---
+++ CLionProjects/embedC/cmake-build-debug/CMakeFiles/embedC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/embedC.dir/main.c.obj"
+  "embedC.exe"
+  "embedC.exe.manifest"
+  "embedC.lib"
+  "embedC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/embedC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
